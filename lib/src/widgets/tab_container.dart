b8043ddab668fd3fd61db016b2e1ca261d22b0be
gsio <gsio@protonmail.com>
2021-11-13 15:02:43 +0700

    Fixed tab background color

--- lib/src/widgets/tab_container.dart
+++ lib/src/widgets/tab_container.dart
@@ -29,7 +29,7 @@ class TabContainer extends StatelessWidget with PreferredSizeWidget {
   @override
   Widget build(BuildContext context) {
     return Material(
-      color: Theme.of(context).colorScheme.primary,
+      color: Theme.of(context).appBarTheme.backgroundColor ?? Theme.of(context).appBarTheme.color,
       child: Center(
         child: TabBar(
           tabs: tabs,
